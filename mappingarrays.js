05025c593115cd22c875f3566a69806b8167203f
Trushna Rabadia <rabadiatrushna@gmail.com>
2022-03-03 14:21:55 +0000

    chapter 16 - arrays and objects

--- mappingarrays.js
+++ mappingarrays.js
@@ -1,11 +1,19 @@
-function generateMessages (nameList) {
-  return nameList.map(name => {
-    return `Hi ${name}! 50% off our best candies for you today!`;
+function generateMessages (namesAndDiscounts) {
+  return namesAndDiscounts.map(info => {
+    return `Hi ${info.name}! ${info.discount}% off our best candies for you today!`;
 
 });
 
 }
 
-const names = ['Anna', 'Laura', 'Josh', 'Min', 'Karla'];
+// const names = ['Anna', 'Laura', 'Josh', 'Min', 'Karla'];
 
-console.log(generateMessages(names))
+const namesAndDiscounts = [
+  { name: 'Anna', discount: 50 },
+  { name: 'Laura', discount: 40 },
+  { name: 'Josh', discount: 30 },
+  { name: 'Min', discount: 50 },
+  { name: 'Karla', discount: 60 }
+];
+
+console.log(generateMessages(namesAndDiscounts))
